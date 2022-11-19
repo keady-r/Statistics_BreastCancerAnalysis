3b03301bb417de63f5330a27e4493839d5326f0a
Ruth School <ruthschool@Ruths-MacBook-Air.local>
2022-11-19 09:57:38 +0000

    Uploading data files with updated lecture content

--- Week2.py
+++ Week2.py
@@ -1,14 +1,14 @@
-#Matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
 
 #Simple Plot
-import matplotlib.pyplot as plt
 
 plt.plot([1,2,3,4,5], [1,4,9,16], 'b.')
 plt.ylabel ('numbers')
 plt.show
 
 #Two Dimensional plot
-import numpy as np
+
 
 x = np.arrange(0.0, 10.0, 0.01)
 y = 3.0 * x
@@ -41,3 +41,27 @@ plt.hist(x)
 plt.show()
 
 #Other plots
+#Scatter
+x1 = np.random.uniform(0.0, 10.0, 100)
+x2 = np.random.uniform(0.0, 5.0, 100)
+x3 = np.random.randint(0.0, 20.0, 100)
+x4 = np.random.normal(0.0, 10.0, 100)
+
+plt.scatter(x1, x2, c-x3, s-x4)
+plt.show()
+
+#sin and cos
+
+x = np.arrange(-2.0 * np.pi, 2.0* np.pi, 0.1)
+
+plt.plot (x, np.sin(x), 'g-')
+plt.plot (x, np.cos(x), 'b-')
+plt.show()
+
+#other addition of labels and legeneds
+x = np.arrange(1.0, 10.0, 0.1)
+
+plt.plot (x, x**2, 'g-', label = 'x^2')
+plt.plot (x, x**3, 'b-', label = 'x^3')
+plt.legend()
+plt.show()
