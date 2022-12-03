76785dff97851397abb4f68eb73b01c47b976584
Ruth School <ruthschool@Ruths-MacBook-Air.local>
2022-12-03 20:09:14 +0000

    Reformated the Jupyter Notebook so that most graphs are pulled in using functions

--- Project/SetUp/PackageInstall.py
+++ Project/SetUp/PackageInstall.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import plotly.express as px
 import plotly.graph_objects as go
+import random
 from plotly.subplots import make_subplots
 from warnings import filterwarnings
 from References import ref
