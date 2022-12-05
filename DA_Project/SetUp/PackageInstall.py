5922d32ddff6c62c739e2496bae46b13117877bf
keady-r <99089546+keady-r@users.noreply.github.com>
2022-12-05 00:46:07 +0000

    Pull request to merge the updated notebooks to main -  issue with merging to main for pull #11 and #12 corrupted the data which had to be manually retrieved.  (#13)
    * Uploading Jupyter Notebook to repo
    
    * Created project folder, commiting the draft of the project concept Hydro-energy as a potential solution for irelands energy crisis
    
    * Updated the dataset and created several graphs showing the data set - drafted version, need to reformat to clearer visibility of data
    
    * Addressing upload issue - removing the venv
    
    * Resetting the concept as the data for hydrolic potential was limited data set
    
    * transferred the graphs from draft.py files to Jupyter, Updated the readMe with reference to new project dataset
    
    * Update to formating, update to read me, additional graphs added to Jupyter Notebbok
    
    * Reformated the Jupyter Notebook so that most graphs are pulled in using functions
    
    * Issue resolved, duplicate clones of repositories were created by mistake. Testing commit
    
    * Updated format of NoteBook
    
    * Final submission
    
    * final submission
    
    Co-authored-by: Ruth School <ruthschool@Ruths-MacBook-Air.local>

---
+++ DA_Project/SetUp/PackageInstall.py
@@ -0,0 +1,24 @@
+#TRENDING PYTHON PACKAGES
+import pandas as pd 
+import numpy as np 
+import sklearn as sk
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.express as px
+import plotly.graph_objects as go
+
+import random
+from plotly.subplots import make_subplots
+
+#WARNINGS 
+import warnings
+from warnings import filterwarnings
+warnings.filterwarnings('ignore')
+
+#REFERENCE TO FUNCTIONS FILE 
+from References import ref
+import sys
+sys.path.insert(1, 'SetUp/')
+import Functions
+from Functions import *
+
